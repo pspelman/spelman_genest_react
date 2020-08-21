0e0855d158d656b89b2e0399ca13bb56beaebbf4
Philip J Spelman <phil_spelman@hotmail.com>
2020-08-21 01:43:34 -0400

    - added algorithms directory - added forLoopsBasics01.js

---
+++ algorithms/forLoopsBasics01.js
@@ -0,0 +1,5 @@
+// this is a basic for loop
+
+for (let i = 0; i < 10; i++) {
+  console.log(``, i);
+}
--- src/practice_loops.js
+++ src/practice_loops.js
@@ -1,7 +1,8 @@
+let name = "phil";
+
 for (let i = 0; i < 10; i++) {
-  console.log(`at ${i}`);
+  console.log(`at ${i} i was ${name}`);
   if (i === 5) {
     console.log(`I'm at `, i);
-
   }
 }
